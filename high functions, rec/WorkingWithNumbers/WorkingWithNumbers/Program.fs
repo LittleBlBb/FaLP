301a5eb0dd0d5cd7413dc2bea08b7899d03a9c1d
LittleBlBb <apdragon2014@gmail.com>
2025-03-18 13:29:46 +0300

    lab5.5 cifrSumDown

--- high functions, rec/WorkingWithNumbers/WorkingWithNumbers/Program.fs
+++ high functions, rec/WorkingWithNumbers/WorkingWithNumbers/Program.fs
@@ -1,12 +1,29 @@
 ﻿open System
 
+let updateVariables n =
+    let n1 = n / 10
+    let cifr = n % 10
+    n1, cifr
+
 let rec cifrSumUp n = 
     if n = 0 then 0
     else (n%10) + (cifrSumUp (n / 10))
+
+let cifrSum n =
+    let rec cifrSum1 n curSum =
+        if n = 0 then curSum
+        else
+            let n1, cifr = updateVariables n
+            let newSum = curSum + cifr
+            cifrSum1 n1 newSum
+    cifrSum1 n 0
 
 [<EntryPoint>]
 let main(args : string[]) = 
     let cifrSumResult = cifrSumUp 12345
     System.Console.WriteLine "Sum of digits: "
     System.Console.WriteLine cifrSumResult
+    let cifrSumDownResult = cifrSum 12345
+    System.Console.WriteLine "Sum of digits: "
+    System.Console.WriteLine cifrSumDownResult
     0
