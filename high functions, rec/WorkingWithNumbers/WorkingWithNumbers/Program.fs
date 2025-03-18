e48a4fc7bf7b86ff5dda7ab1fd21e9af56c0b013
LittleBlBb <apdragon2014@gmail.com>
2025-03-18 13:54:48 +0300

    Lab5.7 numBypass

--- high functions, rec/WorkingWithNumbers/WorkingWithNumbers/Program.fs
+++ high functions, rec/WorkingWithNumbers/WorkingWithNumbers/Program.fs
@@ -30,24 +30,69 @@ let factDown n =
             let n1 = n - 1
             factDown1 n1 newMul
     factDown1 n 1
-
+       
 let choice n f =
     match f with 
-    | true -> cifrSum n
+    | true -> cifrSum
     | false -> factDown n
+
+let cifrMult n =
+    let rec cifrMult1 n curMult =
+        if n = 0 then 0
+        else
+            let n1, cifr = updateVariables n
+            let newMult = curMult * cifr
+            cifrMult1 n1 newMult
+    cifrMult1 n 1
+
+let min n =
+    let rec min1 n curMin =
+        if n = 0 then curMin
+        else
+            let n1, cifr = updateVariables n
+            let newMin = if cifr < curMin then cifr else curMin
+            min1 n1 newMin
+    min1 n 9
+
+let max n =
+    let rec max1 n curMax =
+        if n = 0 then curMax
+        else
+            let n1, cifr = updateVariables n
+            let newMax = if cifr > curMax then cifr else curMax
+            max1 n1 newMax
+    max1 n 0
+
+let numBypass number acc func =
+    accValue + func number
 
 [<EntryPoint>]
 let main(args : string[]) = 
-    let cifrSumResult = cifrSumUp 12345
+    let cifrSumResult = cifrSum 12345
     System.Console.WriteLine "Sum of digits: "
     System.Console.WriteLine cifrSumResult
-    let cifrSumDownResult = cifrSum 12345
-    System.Console.WriteLine "Sum of digits: "
-    System.Console.WriteLine cifrSumDownResult
+    let fact5Result = fact 5
+    System.Console.WriteLine "Fact 5: "
+    System.Console.WriteLine fact5Result
+    let fact5DownResult = factDown 5
+    System.Console.WriteLine "Fact5 Down"
+    System.Console.WriteLine fact5DownResult
     let choiceT = choice 123 true
     System.Console.WriteLine "choiceT"
     System.Console.WriteLine choiceT
     let choiceF = choice 5 false
     System.Console.WriteLine "choiceF"
     System.Console.WriteLine choiceF  
+    let numBypassSumRes = numBypass 123 5 cifrSum
+    System.Console.WriteLine "numBypassSumRes"
+    System.Console.WriteLine numBypassSumRes   
+    let numBypassMulRes = numBypass 513 cifrMult
+    System.Console.WriteLine "numBypassMulRes"
+    System.Console.WriteLine numBypassMulRes   
+    let numBypassMinRes = numBypass 123 5 min
+    System.Console.WriteLine "numBypassMinRes"
+    System.Console.WriteLine numBypassMinRes  
+    let numBypassMaxRes = numBypass 123 5 max
+    System.Console.WriteLine "numBypassMaxRes"
+    System.Console.WriteLine numBypassMaxRes 
     0
