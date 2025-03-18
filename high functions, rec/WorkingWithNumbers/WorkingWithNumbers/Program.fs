e9b99b80e341b005afe34220c807bfe87e87de08
LittleBlBb <apdragon2014@gmail.com>
2025-03-18 13:26:22 +0300

    Lab5.4 cifrSumUp

---
+++ high functions, rec/WorkingWithNumbers/WorkingWithNumbers/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let rec cifrSumUp n = 
+    if n = 0 then 0
+    else (n%10) + (cifrSumUp (n / 10))
+
+[<EntryPoint>]
+let main(args : string[]) = 
+    let cifrSumResult = cifrSumUp 12345
+    System.Console.WriteLine "Sum of digits: "
+    System.Console.WriteLine cifrSumResult
+    0
